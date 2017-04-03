d8c6f2be28e7ebce0c07167fa4551550378cf53b
Louise McMahon <louise@louisemcmahon.co.uk>
2017-04-03 22:33:53 +0100

    nightly

--- src/invoker.js
+++ src/invoker.js
@@ -1,6 +1,27 @@
 'use strict';
 
-module.exports.invoke = function (event, context) {
+module.exports.invoker = function (event, context) {
+	let t = + new Date();
 	let i = 0;
+	let check = true;
+	while (check){
+		// only run 60 times
+		if (i === 60) {
+			check = false;
+			break;
+		//only run once a second
+		}else if (t+1000 < + new Date()){
+			// t = t+1000; and not t = + new Date() as we always slightly slower than 1000ms
+			t = t+1000;
+			i++;
+			invokeRiver();
+		}
+	}
+
+	return true;
 
 };
+
+function invokeRiver() {
+		//TODO invoke function
+}
---
+++ src/river.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Louise on 03/04/2017.
+ */
+
+module.exports = function (event, context) {
+	return true;
+
+};
